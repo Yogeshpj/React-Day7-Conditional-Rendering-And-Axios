4658e01b7067b587d523653ae44253adb808a22b
Yogesh Jayachandran <108916534+Yogeshpj@users.noreply.github.com>
2023-04-05 14:12:09 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import FetchNews from './Api';
+
+function App() {
+  return (
+    <div className="App">
+      <h1><em>FETCHING BOOKS</em></h1>
+      <FetchNews/>
+    </div>
+  );
+}
+
+export default App;
